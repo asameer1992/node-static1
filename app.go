714e0eb91549d3f56d7e1e4bd1b81f222bee8af1
Steve Gattuso <stevenleeg@gmail.com>
2012-09-16 13:08:55 -0400

    Implements create page.

--- app.go
+++ app.go
@@ -65,8 +65,34 @@ func Manage() string {
     return RenderTemplate("manage.mustache", nil)
 }
 
+func Create(ctx *web.Context) string {
+    // Check to see if we're actually publishing
+    title, exists_title := ctx.Params["title"]
+    content, exists_content := ctx.Params["content"]
+
+    var send = map[string]interface{} {
+        "show_success": false,
+    }
+
+    // We are! So let's save it
+    if exists_title && exists_content {
+        // Insert the row
+        _, err := db.Exec("INSERT INTO entries (title, content) VALUES($1, $2)", title, content)
+
+        if err != nil {
+            return RenderTemplate("error.mustache", nil)
+        }
+
+        send["show_success"] = true
+    }
+
+    return RenderTemplate("create.mustache", send)
+}
+
 func main() {
     web.Get("/", Index)
     web.Get("/manage", Manage)
+    web.Get("/manage/create", Create)
+    web.Post("/manage/create", Create)
     web.Run("0.0.0.0:9999")
 }
