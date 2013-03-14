c295e2de25013d0dbdbe89e60d6ae558a971a589
Steve Gattuso <stevenleeg@gmail.com>
2013-03-13 20:12:53 -0400

    Moving ExistingEdit to where it should be

--- views/existing.go
+++ views/existing.go
@@ -1,5 +1,8 @@
 package views
-import "../util"
+import (
+    "../util"
+    "strconv"
+)
 
 func Existing() string {
     db := util.GetDb()
@@ -23,3 +26,25 @@ func Existing() string {
     return util.RenderTemplate("existing.mustache", send)
 }
 
+func ExistingEdit(val string) string {
+    id, err := strconv.Atoi(val)
+    if err != nil {
+        return "Invalid or malformed id"
+    }
+
+    db := util.GetDb()
+    row := db.QueryRow("SELECT id, title, content FROM entries WHERE id=$1 LIMIT 1", id)
+    entry := new(util.Entry)
+    err = row.Scan(&entry.Id, &entry.Title, &entry.Content)
+    if err != nil {
+        return err.Error()
+    }
+
+    send := map[string]interface{} {
+        "Id": entry.Id,
+        "Title": entry.Title,
+        "Content": entry.Content,
+    }
+
+    return util.RenderTemplate("existing_edit.mustache", send);
+}
